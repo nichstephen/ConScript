5864e52f71aac40d8c89f1a2dc689e33d35b069e
nichstephen <stephensonnicholas1@gmail.com>
2024-07-07 11:48:56 -0500

    Add files via upload

---
+++ testscript.js
@@ -0,0 +1,8 @@
+<div>
+    <p>This is a test form element.</p>
+    <script type="text/javascript">
+        (function() {
+            alert('Script loaded and executed successfully!');
+        })();
+    </script>
+</div>
